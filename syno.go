fc03ab713ec7eb5bdb85a24cb88e37b6345a4204
lyderic <lyderic@yoga.lyderic.net>
2017-11-23 07:47:04 +0000

    much improvement to syno

---
+++ syno.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+  "strings"
+  "os"
+)
+
+func init() {
+  log.SetFlags(log.Lshortfile)
+}
+
+func main() {
+
+	word := getWord()
+	synonyms := getSynos(word)
+  for idx,synonym := range synonyms {
+    fmt.Printf("[%03d] %s ", idx, synonym)
+  }
+}
+
+func getWord() string {
+	if len(os.Args) == 1 {
+    log.Fatalln("Please provide a French word as argument!")
+  }
+  return os.Args[1]
+}
+
+func getSynos(word string) []string {
+  var synonyms []string
+	url := fmt.Sprintf("http://www.synonymo.fr/synonyme/%s", word)
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ulSelection := doc.Find("ul")
+  current := ulSelection.Eq(0)
+  liSelection := current.Find("li")
+  for idx := range liSelection.Nodes {
+    word := liSelection.Eq(idx)
+    synonyms = append(synonyms, strings.TrimSpace(word.Text()))
+  }
+  return synonyms
+}
