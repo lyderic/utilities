88d576c31618dc9a9393c9c0740b6696efc7891a
lydericlandry <lydericlandry@penguin>
2018-11-29 14:46:14 +0000

    go fmt run

--- numcpu.go
+++ numcpu.go
@@ -1,17 +1,19 @@
 package main
 
-import(
-  "fmt"
-  "runtime"
+import (
+	"fmt"
+	"runtime"
 )
 
 func main() {
 
-  n := runtime.NumCPU()
-  plural := "s"
-  if n < 2 { plural = "" }
- 
-  fmt.Printf("This system is %s %s and it has %d CPU%s.\n",
-    runtime.GOOS, runtime.GOARCH, n, plural)
+	n := runtime.NumCPU()
+	plural := "s"
+	if n < 2 {
+		plural = ""
+	}
+
+	fmt.Printf("This system is %s %s and it has %d CPU%s.\n",
+		runtime.GOOS, runtime.GOARCH, n, plural)
 
 }
