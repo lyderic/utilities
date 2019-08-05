37107d5fcdf111839d88d7d48369e54617de3eb3
lydericlandry <lydericlandry@penguin>
2019-08-05 10:28:33 +0100

    much inprovements to asciismall

--- asciismall.go
+++ asciismall.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"golang.org/x/text/transform"
-	"golang.org/x/text/unicode/norm"
-	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"unicode"
+
+	"github.com/lyderic/tools"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
 )
+
+var (
+	dryrun bool
+)
+
+type File struct {
+	Name    string
+	Path    string
+	Parent  string
+	Newname string
+	Newpath string
+}
 
 func init() {
 	log.SetFlags(log.Lshortfile)
@@ -17,24 +32,54 @@ func init() {
 
 func main() {
 
-	files, err := ioutil.ReadDir("./")
-	if err != nil {
-		log.Fatal(err)
+	flag.BoolVar(&dryrun, "n", false, "dry-run: simulate action only")
+	flag.Usage = usage
+	flag.Parse()
+	paths := flag.Args()
+
+	if len(paths) == 0 {
+		usage()
+		os.Exit(3)
 	}
 
-	for _, file := range files {
-		name := file.Name()
-		lcname := strings.ToLower(toAscii(name))
-		if lcname == name {
-			fmt.Println(name, ": no change")
-			continue
-		}
-		fmt.Println(name, "->", lcname)
-		err = os.Rename(name, lcname)
+	if dryrun {
+		tools.PrintYellowln("DRY RUN! No change, simulating only!")
+	}
+
+	for _, path := range paths {
+		var file File
+		file.Path = path
+		file.Name = filepath.Base(file.Path)
+		file.Parent = filepath.Dir(file.Path)
+		file.Newname = strings.ToLower(toAscii(file.Name))
+		file.Newpath = filepath.Join(file.Parent, file.Newname)
+		err := process(file)
 		if err != nil {
-			log.Fatal(err)
+			tools.PrintRedf("[FAIL] %v\n", err)
 		}
 	}
+}
+
+func process(file File) (err error) {
+	if _, err = os.Stat(file.Path); os.IsNotExist(err) {
+		return fmt.Errorf("%q: file not found", file.Path)
+	}
+	if file.Newname == file.Name {
+		fmt.Printf("%q: no change\n", file.Name)
+		return
+	}
+	if _, err = os.Stat(file.Newpath); os.IsNotExist(err) {
+		if dryrun {
+			tools.PrintYellowf("[DRY-RUN] %s -> %s\n", file.Path, file.Newpath)
+			return nil
+		} else {
+			fmt.Println(file.Path, "->", file.Newpath)
+			return os.Rename(file.Path, file.Newpath)
+		}
+	} else {
+		fmt.Printf("%q: file already exists. Not overwritten!\n", file.Name)
+	}
+	return
 }
 
 func toAscii(word string) string {
@@ -45,4 +90,10 @@ func toAscii(word string) string {
 
 func isMn(r rune) bool {
 	return unicode.Is(unicode.Mn, r) // Mn: nonspacing marks
+}
+
+func usage() {
+	fmt.Println("Usage: [-n] asciismall <files...>")
+	flag.PrintDefaults()
+	fmt.Println("This program converts file names to lower case and no non-ascii characters")
 }
