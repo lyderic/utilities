d8bb39e9e6d83fcd35d7926fc0bff050ebefd7eb
lydericlandry <lydericlandry@penguin>
2019-12-15 11:32:13 +0000

    improved deployment: now with ansible copy

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
 main() {
-	if [ -z ${1} ] ; then
+	if [ -z ${1} ]; then
 		usage
 	fi
-	gofile="${1}"
-  # utility is gofile without the '.go' extension
-	utility="${gofile%.*}"
-	go build -v "${gofile}" && mv -v "${utility}" ~/bin
-	if [ $? -eq 0 ] ; then
-		echo "${utility} deployed"
+	utility=${1%.*}
+	echo "Utility: ${utility}"
+	echo '[building linux amd64]'
+	build amd64 x86_64
+	echo '[building linux arm]'
+	build arm armv7l
+	echo '[ansible deployment]'
+	copy-to-remote
+}
+
+copy-to-remote() {
+	echo "ANSIBLE: utility=${utility}"
+	cd ${HOME}/repositories/ansible
+	ansible all -o -m copy -a "src=/dev/shm/${utility}-{{ansible_architecture}} dest=~{{operator}}/bin/${utility} mode=0755"
+}
+
+build() {
+	goarch=${1}
+	ansiblearch=${2}
+	GOARCH=${goarch} \
+	go build -v -o "/dev/shm/${utility}-${ansiblearch}" "${utility}.go"
+	if [ ${goarch} == 'arm' ] ; then
+		ln -sfv /dev/shm/${utility}-armv7l /dev/shm/${utility}-armv6l
 	fi
 }
 
 usage() {
-	echo "Usage: $(basename ${0}) <utility.go>"
+	echo "Usage: deploy.sh <utility.go>"
 	exit 23
 }
 
