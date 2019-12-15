780a0dee0bf95dbc60e851c29056ff5b92281625
lydericlandry <lydericlandry@penguin>
2019-12-15 08:50:22 +0000

    added deump

---
+++ deump.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
+)
+
+var (
+	keys []string
+)
+
+func init() {
+	keys = []string{
+		"AWS",
+		"B2",
+		"CLOUDFLARE",
+		"LPASS",
+		"LPPK",
+		"OBSCURA",
+		"RCLONE",
+		"RESTIC",
+		"SSA",
+		"SSH",
+		"SUBB",
+		"TMOUT",
+		"VAULT",
+		"p",
+	}
+}
+
+func main() {
+	deump()
+}
+
+func deump() {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	for _, envvar := range os.Environ() {
+		for _, key := range keys {
+			if strings.HasPrefix(envvar, key) {
+				bits := strings.Split(envvar, "=")
+				fmt.Fprintf(w, "%s\t%s\n", bits[0], bits[1])
+			}
+		}
+	}
+	w.Flush()
+}
