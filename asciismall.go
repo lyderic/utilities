7121a34d68a1c40614cf9ec3d63259b8a74b9784
lyderic <lyderic@tintin.lyderic.net>
2020-11-25 13:00:11 +0000

    asciismall is now a filter, à la cat

--- asciismall.go
+++ asciismall.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 	"unicode"
 
-	"github.com/lyderic/tools"
 	"golang.org/x/text/transform"
 	"golang.org/x/text/unicode/norm"
-)
-
-var (
-	dryrun bool
 )
 
 func init() {
@@ -23,70 +17,20 @@ func init() {
 }
 
 func main() {
-	flag.BoolVar(&dryrun, "n", false, "dry-run: simulate action only")
-	flag.Usage = usage
-	flag.Parse()
-	inputs := flag.Args()
-	if len(inputs) == 0 {
-		usage()
-		os.Exit(3)
-	}
-	if dryrun {
-		tools.PrintYellowln("DRY RUN! No change, simulating only!")
-	}
-	for _, input := range inputs {
-		finfo, err := os.Stat(input)
-		if err != nil {
-			tools.PrintRedf("%q: invalid input. Skipping...\n", input)
-			continue
+	if len(os.Args) > 1 {
+		for _, args := range os.Args[1:] {
+			process(args)
 		}
-		if finfo.IsDir() {
-			err = processDir(input)
-		} else {
-			err = processPath(input)
-		}
-		if err != nil {
-			tools.PrintRedf("Error processing input %q: %v\n", input, err)
-			continue
-		}
-	}
-}
-
-func processDir(dirpath string) (err error) {
-	err = filepath.Walk(dirpath, func(path string, finfo os.FileInfo, err error) error {
-		if finfo.IsDir() {
-			fmt.Printf("%q is a directory: conversion skipped...\n", path)
-			return nil
-		}
-		return processPath(path)
-	})
-	return
-}
-
-func processPath(path string) (err error) {
-	if _, err = os.Stat(path); os.IsNotExist(err) {
-		return fmt.Errorf("%q: file not found", path)
-	}
-	name := filepath.Base(path)
-	parent := filepath.Dir(path)
-	newname := strings.ToLower(toAscii(name))
-	newpath := filepath.Join(parent, newname)
-	if newname == name {
-		fmt.Printf("%q: no change\n", name)
 		return
 	}
-	if _, err = os.Stat(newpath); os.IsNotExist(err) {
-		if dryrun {
-			tools.PrintYellowf("[DRY-RUN] %s -> %s\n", path, newpath)
-			return nil
-		} else {
-			fmt.Println(path, "->", newpath)
-			return os.Rename(path, newpath)
-		}
-	} else {
-		fmt.Printf("%q: file already exists. Not overwritten!\n", name)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		process(scanner.Text())
 	}
-	return
+}
+
+func process(input string) {
+	fmt.Println(strings.ToLower(toAscii(input)))
 }
 
 func toAscii(word string) string {
@@ -97,11 +41,4 @@ func toAscii(word string) string {
 
 func isMn(r rune) bool {
 	return unicode.Is(unicode.Mn, r) // Mn: nonspacing marks
-}
-
-func usage() {
-	fmt.Println("Usage: [-r] [-n] asciismall <files...> <directories...>")
-	flag.PrintDefaults()
-	fmt.Println("This program converts file names to lower case and no non-ascii characters")
-	fmt.Println("Directories are processed recursively. Directory names are NOT converted")
 }
