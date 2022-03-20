faabc23d8123f2a8a5c1099061f00136f58abb89
lyderic <lyderic@tintin.lyderic.net>
2022-03-20 08:48:37 +0100

    synonyms now on two colums

--- syno/go.mod
+++ syno/go.mod
@@ -2,9 +2,24 @@ module syno
 
 go 1.18
 
-require github.com/PuerkitoBio/goquery v1.8.0
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/jedib0t/go-pretty/v6 v6.2.7
+	github.com/lyderic/tools v0.3.8
+)
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sendgrid/rest v2.6.8+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.11.0+incompatible // indirect
+	github.com/wayneashleyberry/terminal-dimensions v1.1.0 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- syno/main.go
+++ syno/main.go
@@ -4,46 +4,66 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+
+	. "github.com/lyderic/tools"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/jedib0t/go-pretty/v6/table"
 )
-
-type Synonym struct {
-	Index int
-	Word  string
-}
 
 func init() {
 	log.SetFlags(log.Lshortfile)
 }
 
 func main() {
-	word := getWord()
-	synonyms := getSynos(word)
-	for _, synonym := range synonyms {
-		fmt.Printf("[%03d] %s ", synonym.Index, synonym.Word)
-	}
-	fmt.Println()
-}
-
-func getWord() string {
 	if len(os.Args) == 1 {
 		log.Fatalln("Please provide a French word as argument!")
 	}
-	return os.Args[1]
+	synonyms := getSynonyms(os.Args[1:])
+	ln := len(synonyms)
+	fmt.Printf("found %d synonym%s:\n", ln, Ternary(ln > 1, "s", ""))
+	/*
+		for idx, synonym := range synonyms {
+			Green("[%02d:%s]", idx, synonym)
+		}
+		fmt.Println()
+	*/
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	//t.SetStyle(table.StyleLight)
+	t.SetStyle(NoStyle)
+	for i := 0; i < len(synonyms); i++ {
+		one := synonyms[i]
+		two := ""
+		if i < ln-1 {
+			two = synonyms[i+1]
+		}
+		t.AppendRow(table.Row{one, two})
+		i++
+	}
+	t.Render()
 }
 
-func getSynos(word string) (synonyms []Synonym) {
+func getSynonyms(args []string) (synonyms []string) {
+	word := strings.Join(args, "%20")
 	url := fmt.Sprintf("https://www.cnrtl.fr/synonymie/%s", word)
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	doc.Find(".syno_format").Each(func(i int, s *goquery.Selection) {
-		var synonym Synonym
-		synonym.Index = i + 1
-		synonym.Word = s.Text()
-		synonyms = append(synonyms, synonym)
+		synonyms = append(synonyms, s.Text())
 	})
 	return
+}
+
+var NoStyle = table.Style{
+	Name:    "NoStyle",
+	Box:     table.StyleBoxDefault,
+	Color:   table.ColorOptionsDefault,
+	Format:  table.FormatOptionsDefault,
+	HTML:    table.DefaultHTMLOptions,
+	Options: table.OptionsNoBordersAndSeparators,
+	Title:   table.TitleOptionsDefault,
 }
